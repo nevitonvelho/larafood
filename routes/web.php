15dce8dc1cbb8ffe827e1b5c730d80759dfed2b9
Your Name <nevitonvelho01@gmail.com>
2022-06-21 20:16:17 -0300

    novo

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/admin/plans', 'Admin\PlanController@index')->name('plans.index');
+
+// Route::get('/admin/plans', [Admin\PlanController::class, 'plans.index']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
