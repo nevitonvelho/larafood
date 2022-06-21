99386d02b2087e613444a1173cb2e6e2642fe2f5
Your Name <nevitonvelho01@gmail.com>
2022-06-21 20:32:28 -0300

    neviton

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin/plans', 'Admin\PlanController@index')->name('plans.index');
+Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
 
 // Route::get('/admin/plans', [Admin\PlanController::class, 'plans.index']);
 
