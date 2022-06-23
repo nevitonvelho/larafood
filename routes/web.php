6f401e0351435e6c27ea7628c43719f004755c26
Your Name <nevitonvelho01@gmail.com>
2022-06-23 12:08:26 -0300

    erro em rotas resolvido OK

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\{
+    PlanController
+};
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
+Route::get('/admin/plans', [PlanController::class, 'index'])->name('plans.index');
 
 // Route::get('/admin/plans', [Admin\PlanController::class, 'plans.index']);
 
